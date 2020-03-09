2ce154d045592698f8d9cf6d17d91f64c85af95f
Arthur Dang <arthur.dang@supelec.fr>
2020-03-09 16:11:42 +0100

    tp4

--- expr/cfg_dead_assign_elimination.c
+++ expr/cfg_dead_assign_elimination.c
@@ -8,10 +8,30 @@
 
 int num_changed = 0;
 int roundnr = 0;
+int dead_code_detected;
 
 void dead_assign_elimination_graph(list* live, cfg* c){
+  dead_code_detected = 0;
+  struct list* assign_var = (struct list*)malloc(sizeof(struct list));
+  while(c){
+    if(c->node->type == NODE_ASSIGN){
+      assign_var->elt = c->node->assign.var;
+      assign_var->next = NULL;
+      if(!list_string_incl(assign_var, live_after(c->node, live))){
+        // if the assigned variable is dead after the assign node
+        dead_code_detected = 1;
+        c->node->type = NODE_GOTO;
+        c->node->goto_succ = c->node->assign.succ;
+      }
+    }
+    c = c->next;
+  }
 }
 
 cfg_prog* dead_assign_elimination_prog(cfg_prog* c){
+  dead_code_detected = 1;
+  while(dead_code_detected){
+    dead_assign_elimination_graph(liveness_prog(c), c->graph);
+  }
   return c;
 }
--- expr/cfg_liveness.c
+++ expr/cfg_liveness.c
@@ -15,27 +15,131 @@ void print_mapping(FILE* f, list* map){
   }
 }
 
+void concatenate(struct list* a,struct list* b)
+{
+  if (a->next == NULL)
+    a->next = b;
+  else
+    concatenate(a->next,b);
+}
+
 list* expr_used_vars(struct expression* e){
- return NULL;
+  struct list* used_var = (struct list*)malloc(sizeof(struct list));
+  switch(e->etype){
+    case EUNOP: {
+      struct list *used_var3 = (struct list *) malloc(sizeof(struct list));
+      used_var3 = expr_used_vars(e->unop.e);
+      if (used_var3->elt == NULL) {
+        used_var->elt = NULL;
+        used_var->next = NULL;
+      } else {
+        used_var = used_var3;
+      }
+    }
+    case EBINOP:
+    {
+      struct list* used_var1 = (struct list*)malloc(sizeof(struct list));
+      struct list* used_var2 = (struct list*)malloc(sizeof(struct list));
+      used_var1 = expr_used_vars(e->binop.e1);
+      used_var2 = expr_used_vars(e->binop.e2);
+      if(used_var1->elt == NULL && used_var2->elt == NULL){
+        used_var->elt = NULL;
+        used_var->next = NULL;
+      } else if(used_var1->elt == NULL) {
+        used_var = used_var2;
+      } else if(used_var2->elt == NULL){
+        used_var = used_var1;
+      } else {
+        concatenate(used_var1, used_var2);
+        used_var = used_var1;
+      }
+    }
+    case EINT:
+      used_var->elt = NULL;
+      used_var->next = NULL;
+    case EVAR:
+      used_var->elt = e->var.s;
+      used_var->next = NULL;
+  }
+ return used_var;
 }
 
 
 list* live_after(node_t* n, list* map){
   list* live_aft = NULL;
+  struct list* successeurs = (struct list*)malloc(sizeof(struct list));
+  successeurs = succs_node(n);
+  void* successeur_id = successeurs->elt;
+  while(successeurs){
+    successeur_id = successeurs->elt;
+    if(live_aft == NULL) {
+      live_aft = assoc_get(map, successeur_id);
+    } else {
+      concatenate(live_aft, assoc_get(map, successeur_id));
+    }
+    successeurs = successeurs->next;
+  }
   return live_aft;
 }
 
 list* live_before(list* live_aft, node_t* n){
-  return NULL;
+  list* list_bef = NULL;
+
+  switch(n->type){
+    case NODE_ASSIGN:
+    {
+      struct list* used_var = (struct list*)malloc(sizeof(struct list));
+      used_var = expr_used_vars(n->assign.e);
+      concatenate(list_bef, used_var);
+    }
+    case NODE_PRINT:
+    {
+      struct list* used_var = (struct list*)malloc(sizeof(struct list));
+      used_var = expr_used_vars(n->print.e);
+      concatenate(list_bef, used_var);
+    }
+    case NODE_RETURN:
+    {
+      struct list* used_var = (struct list*)malloc(sizeof(struct list));
+      used_var = expr_used_vars(n->ret.e);
+      concatenate(list_bef, used_var);
+    }
+    case NODE_COND:
+    {
+      struct list* used_var = (struct list*)malloc(sizeof(struct list));
+      used_var = expr_used_vars(n->cond.cond);
+      concatenate(list_bef, used_var);
+    }
+    case NODE_GOTO:
+      return live_aft;
+  }
+  return list_bef;
 }
 
 int new_changes;
 
 list* liveness_graph(list* map, cfg* c){
+  new_changes = 0;
+  while(c){
+    struct list* live_aft = (struct list*)malloc(sizeof(struct list));
+    live_aft = live_after(c->node, map);
+    struct list* live_bef = (struct list*)malloc(sizeof(struct list));
+    live_bef = live_before(live_aft, c->node);
+    if(!list_string_incl(live_bef, assoc_get(map, &(c->id)))){
+      new_changes = 1;
+      assoc_set(map, &(c->id), live_bef);
+    }
+
+    c = c->next;
+  }
   return map;
 }
 
 list* liveness_prog(cfg_prog* p){
+  new_changes = 0;
   list* map = NULL;
+  while(!new_changes){
+    map = liveness_graph(map, p->graph);
+  }
   return map;
 }
